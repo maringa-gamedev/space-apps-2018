c8749790119d7e7e57cbae3733cb9e2bdac5d0e5
Tiago Nascimento <tiago@theosirian.com>
2018-10-21 01:09:19 -0300

    first screen without scripts

---
+++ Assets/Scripts/MapLocation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapLocation : MonoBehaviour {
+
+    [SerializeField]
+    public int minYear;
+
+    [SerializeField]
+	public string locationName;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
